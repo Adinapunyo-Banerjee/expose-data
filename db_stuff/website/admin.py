22f4b812f69cba3a4e81547dafca6de9242851a0
Adinapunyo Banerjee <adinapunyo@gmail.com>
2022-09-10 22:11:06 +0530

    Registered DB to Admin's panel

--- db_stuff/website/admin.py
+++ db_stuff/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Member
 
 # Register your models here.
+
+admin.site.register(Member)
+
+# This will show you the tables present in the DB with their respective elements.
