623afef0d1dcf99a9d399fd8745a5c2ce2dd43c1
Adinapunyo Banerjee <adinapunyo@gmail.com>
2022-09-10 22:00:56 +0530

    Member Table made in DB

--- db_stuff/website/models.py
+++ db_stuff/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+
+# In SQL we would define the type of data in the table here, Integer, Text, Null, whatever. We do the same here! A table named "Member" is created here.
