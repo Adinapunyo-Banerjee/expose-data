44e9f1f0d16344a59caa23760855570d65be1aa4
Adinapunyo Banerjee <adinapunyo@gmail.com>
2022-09-11 12:12:48 +0530

    Spits out Database data in home page

--- db_stuff/website/models.py
+++ db_stuff/website/models.py
@@ -6,11 +6,14 @@ class Member(models.Model):
     lname = models.CharField(max_length=30)
     email = models.EmailField(max_length=50)
     passwd = models.CharField(max_length=50)
-    age = models.IntegerField
+    age = models.IntegerField()
 
-# In SQL we would define the type of data in the table here, Integer, Text, Null, whatever. We do the same here! A table named "Member" is created here.
+# In SQL we would define the type of data in the table here, Integer, Text, Null, whatever.
+# We do the same here! A table named "Member" is created here.
 
     def __str__(self):
         return self.fname + ' ' + self.lname + ' - ' + self.email
 
-# Doing this will allow the table in admin panel to not show "Member Model (i)" and instead give their name and email!
+# Doing this will allow the table in admin panel to not show "Member Model (i)" and instead give their name and email! (Changes object display name)
+
+# This __str__ function returns the same thing when we are trying to access it in views.py
--- db_stuff/website/views.py
+++ db_stuff/website/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    all_members = Member.objects.all
+    return render(request, 'index.html', {'all':all_members})
+
+# def join(request):
+#     return render(request, 'join.html', {})
