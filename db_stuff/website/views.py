b680150888128892c6d374958552f70294878de8
Adinapunyo Banerjee <adinapunyo@gmail.com>
2022-09-11 20:50:05 +0530

    Completely working now yay!

--- db_stuff/website/views.py
+++ db_stuff/website/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Member
 from .forms import Memberform
 
@@ -13,7 +14,18 @@ def join(request):
         form = Memberform(request.POST or None)
         if form.is_valid():
             form.save()
-        return render(request, 'join.html', {})
+        else:
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            age = request.POST['age']
+            email = request.POST['email']
+            passwd = request.POST['passwd']
+
+            messages.error(request, 'Given information is not valid! Check all fields.')
+            return render(request, 'join.html', {fname:'fname', lname:'lname', age:'age', email:'email', passwd:'passwd'})
+
+        messages.success(request, 'Thanks for Signing Up!')
+        return redirect('home')
         
     else:
         return render(request, 'join.html', {})
